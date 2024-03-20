4ab824a4d01ae2390dfcacfb5b28a0a3ec504fa6
Joko Gadingan <87678905+jk3570@users.noreply.github.com>
2024-03-20 18:33:34 +0800

    ready to deploy

---
+++ models/userModel.js
@@ -0,0 +1,150 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+
+const Schema = mongoose.Schema
+
+// User structure in database
+const userSchema = new Schema({
+  role: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'LGBTQ', 'Prefer not to say'],
+    required: true
+  },
+  birthdate: {
+    type: Date,
+    required: true
+  },
+  region: {
+    type: String,
+    required: true
+  },
+  province: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  barangay: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+
+})
+
+// Static signup method
+userSchema.statics.signup = async function(
+  role,
+  username,
+  firstname,
+  lastname,
+  gender,
+  birthdate,
+  region,
+  province,
+  city,
+  barangay,
+  email,
+  password) {
+  
+  // Validation of all fields
+  if (!role || !username || !firstname || !lastname || !gender || !birthdate || !region || !city || !email || !password ) {
+    throw Error('All fields are required')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
+
+  const exists = await this.findOne({ email })
+  if (exists) {
+    throw Error('Email already in use')
+  }
+  
+  const usernameExists = await this.findOne({ username })
+  if (usernameExists) {
+    throw Error('Username already in use')
+  }
+
+  //initialize the hashing attribute
+  const salt = await bcrypt.genSalt(10)
+
+  //hashing all the users data
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  // Inserting data to database
+  const user = await this.create({ 
+    role,
+    username,
+    firstname,
+    lastname,
+    gender,
+    birthdate,
+    region,
+    province,
+    city,
+    barangay,
+    email,
+    password: passwordHash
+    })
+
+  return user
+}
+
+
+// Get the user from the database
+userSchema.statics.login = async function(identifier, password) {
+
+  if (!identifier || !password) {
+    throw Error('All fields are required')
+  }
+
+  const user = await this.findOne({
+    $or: [{ username: identifier }, { email: identifier }]
+  })
+  
+  // .select('username email firstname lastname gender birthdate region province city barangay') //get the user's information from the database
+
+  if (!user) {
+    throw Error('Incorrect username or email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
+
+  return user
+}
+
+module.exports = mongoose.model('User', userSchema)
---
+++ server.js
@@ -0,0 +1,89 @@
+// import modules
+const fs = require("fs");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors"); // Import the cors module
+const userRoutes = require("./routes/user");
+const OpenAI = require("openai");
+require("dotenv").config();
+const path = require("path");
+
+const port = process.env.PORT || 4000;
+
+// express app
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// routes
+app.use("/user", userRoutes);
+
+// OpenAI's API
+const openai = new OpenAI({
+  apiKey: process.env.AI_API,
+});
+
+app.post("/chat", async (req, res) => {
+  try {
+    let messages = [];
+    // Read the JSON file containing messages
+    fs.readFile("./messages.json", "utf8", async (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      // Parse the JSON data
+      messages = JSON.parse(data);
+      if (!req.body.messages) {
+        req.body.messages = [];
+      }
+
+      try {
+        // Call OpenAI API with messages from JSON file and request body
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: messages.concat(req.body.messages), // Concatenate JSON file messages with request messages
+          temperature: 1,
+          max_tokens: 4095,
+          top_p: 1,
+          frequency_penalty: 0.54,
+          presence_penalty: 0.49,
+        });
+
+        // Send response from OpenAI API
+        res.json(response.data);
+      } catch (error) {
+        // Handle OpenAI API call error
+        res.status(500).json({ error: error.message });
+      }
+    });
+  } catch (error) {
+    // Handle file read error
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Wildcard route to serve the index.html file for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
+// connect to db
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(port, () => {
+      console.log("Connected to Database (MongoDB Atlas) & listening on Port: ", port);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = app;
